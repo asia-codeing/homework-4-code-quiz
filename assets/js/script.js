d21f2ffafa334913bf7b97bbaed79fc0a13e35a0
Asia Alnahi <asia.alius@gmail.com>
2021-02-26 12:26:49 -0800

    create the answers and add the score to the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,21 @@ var startButton = document.querySelector("#startBtn");
 var countdown = document.querySelector("#countdown");
 var timer;
 var timerCount;
+var index = 0;
 var currentQuestionIndex;
 var randomQuestions;
 var questionEl = document.querySelector("#question");
 var answerBtnEl = document.querySelector("#answer-buttons");
 var highScore = document.querySelector("#score-amount");
-var score;
+var score = 0;
+var initial;
+var inputInitial = document.querySelector("text");
+var submitInitial = document.querySelector("#initial-submit")
+var submitBtn = document.querySelector(".btn");
+var goBack = document.querySelector(".goback-btn");
+var initialScore = document.querySelector("#score-output");
+var clear = document.querySelector(".clear-btn");
+
 //addEventListener to the start button and put the startQuiz function in side
 startButton.addEventListener('click', startQuiz);
 function startQuiz(){
@@ -24,37 +33,50 @@ function startQuiz(){
     timerCount = 20;
     startTimer()
     showQestion()
-    setNextQuesion()
+    
 }
 // this function to set the  questions
 function showQestion(){
     questionEl.innerHTML = randomQuestions.question;
-    /*answerBtnEl.text = randomQuestions.answers[0].text; 
-    answerBtnEl.text = randomQuestions.answers[1].text; 
-    answerBtnEl.text = randomQuestions.answers[2].text; 
-    answerBtnEl.text = randomQuestions.answers[3].text; */
-    
+
     for (var i = 0; i < randomQuestions.answers.length; i++) {
         console.log(randomQuestions.answers[i].text);
-        document.querySelector(`.btn-${i}`).textContent = randomQuestions.answers[i].text;
-
+        var button = document.createElement("button");
+        button.setAttribute("style", " background-color:#c9edff; padding: 20px;font-family: 'Courier New', Courier, monospace;font-weight: bold;font-size: 16;border-radius: 8px; margin:20px;");
+    
+        button.textContent = randomQuestions.answers[i].text;
+        answerBtnEl.appendChild(button);
+        
     }
+        answerBtnEl.addEventListener("click", function(){
+        if (randomQuestions.answers.correct = true){
+            score ++;
+            localStorage.setItem("score", JSON.stringify(score));
+           // localStorage.setItem("score", score);
+            alert('Correct!');
+        }else{
+            timerCount - 5;
+            alert('Wrong');
+        }
+        setNextQuesion(1)
+    });
+    
 }
-function setNextQuesion() {
+function setNextQuesion(next) {
+       answerBtnEl.addEventListener("click", function(event) {
+       event.stopPropagation();
+        index = index + next;
+        if (index < 0) { 
+          index =  randomQuestions.answers.length ; 
+        } else if (index >  randomQuestions.answers.length ) { 
+          index = 0;
+        }
+        currentQuestionIndex = randomQuestions.answers[index];
+      
+      });      
  
-
 }
 
-function selectAnswer() {
-    if (correct = true){
-        score ++;
-        localStorage.setItem("score", JSON.stringify(score));
-        alert('Correct!');
-    }else{
-        timerCount - 5;
-        alert('Wrong');
-    }
-}
 var questionsArr = [
     {
         question: 'what is 5 + 5?',
@@ -83,7 +105,6 @@ var questionsArr = [
             {text: '64', correct: false}
         ]
     }
-
 ]
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
@@ -94,14 +115,65 @@ function startTimer() {
       if (timerCount === 0) {
         // Clears interval
         clearInterval(timer);
+        allDone();
       }
     }, 1000);
   }
 
-  function submit(){
+ function allDone(){
     document.querySelector("#submit").style.visibility = "visible";
     document.querySelector("#questions-container").style.visibility = "hidden";
+    var score = JSON.parse(localStorage.getItem("score"));
+    highScore.textContent = score;
+    //localStorage.setItem("initial", JSON.stringify(initial));
+    storeInitial()
+
+    var scoreAndInitial ={
+        score : score.value,
+        initial : initial.value.trim()
+    }
+    localStorage.setItem("scoreAndInitial", JSON.stringify(scoreAndInitial));
 
   }
+  //this function save the inital in the localstorage
+  function storeInitial() {
+    //event.preventDefault();
+    localStorage.setItem("initial", JSON.stringify(initial));
+  }
+  /*
+  submitBtn.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    var initialText = inputInitial.value.trim();
+  
+    // Return from function early if submitted initialText is blank
+    if (initialText === "") {
+      return;
+    }
+    storeInitial()
+    document.querySelector("#submit").style.visibility = "hidden";
+    document.querySelector("#highscores").style.visibility = "visible";
+  });*/
+  
+  //Submition function for Initial
+  submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    document.querySelector("#submit").style.visibility = "hidden";
+    document.querySelector("#highscores").style.visibility = "visible";
+    var sI = JSON.parse(localStorage.getItem("scoreAndInitial"));
+    initialScore.textContent = sI;
+    });
+
+ //Go back button
+ goBack.addEventListener("click", function(){
+     document.querySelector("#highscores").style.visibility = "hidden";
+     document.querySelector(".startingPage").style.visibility = "visible";
+ });
+ //Clear button
+ clear.addEventListener("click", function() {
+     initialScore.textContent = '';
+ })
+  
+
 
 
